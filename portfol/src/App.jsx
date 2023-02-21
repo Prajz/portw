eff346fa9f81812e35343838e2bdca4e485696fe
Prajz <d69praj@gmail.com>
2023-02-21 20:38:38 +0000

    added header + about me

--- portfol/src/App.jsx
+++ portfol/src/App.jsx
@@ -1,11 +1,12 @@
-import {ScrollContainer, ScrollPage, Animator, batch, Fade, Move, Sticky, MoveOut,MoveIn} from 'react-scroll-motion'
+import {ScrollContainer, ScrollPage, Animator, batch, Fade, Move, Sticky, MoveOut, MoveIn} from 'react-scroll-motion'
 import './index.css';
 import AnimatedText from "./AnimatedTextsingle";
 import AnimatedTexts from "./AnimatedTextsinglelong";
 import {motion, useScroll, useSpring} from 'framer-motion'
 import React from 'react';
 import Dropd from './components/dropd/Dropd.jsx'
-
+import Header from './components/header/Header.jsx'
+import About from './components/about/About.jsx'
 function App() {
   const FadeUp = batch(Fade(), Move(), Sticky());
   const FadeUpn = batch(Fade(), Move());
@@ -48,6 +49,42 @@ const container = {
   return (
     <div className='aap'>
       <Dropd />
+      <motion.div className="progress-bar" style={{ scaleX }} />
+      <ScrollContainer snap='proxmity'>
+        <ScrollPage>
+          <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -250))}>
+            <Header />
+            <motion.div
+                className="scrolld"
+                initial="hidden"
+                whileInView="visible"
+                variants={container}>
+                  <div className="bongte">
+                    {placeholderText.map((item, index) => {
+                      return <AnimatedText {...item} key={index} />;
+                    })}
+                  </div>
+                </motion.div>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage>
+          <Animator animation={FadeUp}>
+          <Animator className='aboum' 
+          animation={MoveIn(-500, 0)}> <Animator animation={MoveOut(500,0)} ><h1>ABOUT ME</h1></Animator></Animator>
+           <Animator animation={FadeUpn}>
+            <About /> 
+           </Animator>
+          </Animator>
+          <Animator><motion.div className='scrollpro'
+          initial="hidden"
+          whileInView="visible"
+          variants={containerr}>
+                      {scrollproj.map((item, index) =>{
+                        return <AnimatedTexts {...item} key={index} />;
+                      })}</motion.div>
+                      </Animator>
+        </ScrollPage>
+        </ScrollContainer>
     </div>
   )
 }
---
+++ portfol/src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import './Header.css'
+
+
+
+const Header = () => {
+  
+
+  return (
+    <div className ="homepage">
+    <div className='hedcon'>
+        <h5>Hi, I'm</h5>
+        <h1 className='miname' data-value="Dev Prajapati">Dev Prajapati</h1>
+        <h5 className='text-light'> Software Developer</h5>
+    </div>
+    </div>
+
+  )
+}
+
+export default Header
